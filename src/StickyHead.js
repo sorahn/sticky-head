ecdda4243d357f76be0edaa2fce72868d769fdff
Daryl Roberts <daryl.roberts@gmail.com>
2017-10-10 23:52:09 -0700

    test

--- src/StickyHead.js
+++ src/StickyHead.js
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Comment for comments sake
 class TR extends React.Component {
   updateCellWidth = i => width => {
     this.props.updateColumnWidth(width, i);
